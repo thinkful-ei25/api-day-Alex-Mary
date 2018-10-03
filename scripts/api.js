69e55e6e58fb7645a5203e1814afb7760c7e5685
mkcnly <mkcnly@gmail.com>
2018-10-03 09:42:24 -0700

    finished exercise 3

--- scripts/api.js
+++ scripts/api.js
@@ -5,7 +5,18 @@ const api = (function() {
   const getItems = function(callback) {
     $.getJSON(`${BASE_URL}/items`, callback);
   };
+  const createItem = function(name, callback){
+  const newItem = JSON.stringify({name,});
+  $.ajax({
+    url: `${BASE_URL}/items`,
+    method: 'POST',
+    contentType: 'application/json',
+    data: newItem,
+    success: (callback)
+  });
+  }
   return {
+    createItem,
     getItems
   };
 })();
--- scripts/index.js
+++ scripts/index.js
@@ -8,3 +8,10 @@ $(document).ready(function() {
 });
 
 store.items.push(Item.create('apples'));
+
+
+api.createItem('pears', (newItem) => {
+  api.getItems((items) => {
+    console.log(items);
+  });
+});
